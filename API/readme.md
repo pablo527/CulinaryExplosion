2eca26b1662432854cf883929e3ae18b1318e4ce
PABLO <pablo_7v.m@hotmail.com>
2023-06-12 22:47:38 -0500

    merge with trunk

---
+++ API/readme.md
@@ -0,0 +1,37 @@
+crear entorno virtual
+
+    virtualenv venv
+
+install django
+
+    pip3 install django
+    pip install djangorestframework
+
+    django-admin --version
+
+crear proyecto
+
+    django-admin startproject api .
+
+inicializar proyecto
+
+    python manage.py runserver.py    
+
+
+Crear app
+
+    python manage.py startapp app
+    agregar a settings
+
+mIgraciones de models
+
+    python manage.py makemigrations
+
+
+crear tabla
+
+    python manage.py migrate
+
+
+
+
---
+++ Fronted/src/app/restaurants/pages/restaurant-page/restaurant-page.component.ts
@@ -0,0 +1,117 @@
+import { Component, OnInit } from '@angular/core';
+import { RestaurantResponse } from '../../interface/restaurant.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { RestaurantService } from '../../service/restaurant.service';
+import { GaleryService } from '../../service/galery/galery.service';
+import { Galery } from '../../models/galery';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { VoteResponse } from '../../interface/valueVote.interface';
+
+
+
+@Component({
+  selector: 'app-restaurant-page',
+  templateUrl: './restaurant-page.component.html',
+  styleUrls: ['./restaurant-page.component.css'],
+})
+export class RestaurantPageComponent implements OnInit {
+
+  public rest!: RestaurantResponse;
+  galery!: Galery[];
+  images!: any[];
+  ratingValue!: number;
+  position!: string;
+  display: boolean = false;
+  displayBye: boolean = false;
+  personName !: string;
+  personCel !: string;
+  public votes :VoteResponse[] = [];
+  public formUser: FormGroup;
+  onRatingChange(event: any) {
+    this.ratingValue = event.value;
+
+  }
+
+    responsiveOptions:any[] = [
+        {
+            breakpoint: '1024px',
+            numVisible: 5
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1
+        }
+    ];
+  constructor(
+    private restaurantService: RestaurantService,
+    private activateRoute: ActivatedRoute,
+    private router: Router,
+    private galeryService: GaleryService,
+    private formBuilder: FormBuilder,
+  ) {
+    this.formUser = this.formBuilder.group({
+      personName: ['', [Validators.required, Validators.pattern('[A-Za-z]+')]],
+      personId: ['', [Validators.required, Validators.pattern('[0-9]+')]]
+    });
+
+
+  }
+
+  ngOnInit(): void {
+    this.activateRoute.params
+      .pipe(
+        switchMap(({ id }) => this.restaurantService.getRestaurantById(id)),
+      ).subscribe(rest => {
+        if (!rest) return this.router.navigate(['/restaurant/list']);
+        this.rest = rest;
+        //  console.log({rest})
+        return
+      })
+
+      // this.galeryService.getGalery().then((galery: Galery[]) =>{
+      //   this.galery = galery;
+      // })
+
+      // this.galeryService.getGalery().subscribe((data: Galery[])=>{
+      //   this.galery = data.filter(galery => galery.id === this.rest);
+      // })
+  }
+
+  goBack() {
+    this.router.navigateByUrl('restaurants/list')
+  }
+
+  openModal() {
+    if(this.ratingValue === undefined){
+      this.ratingValue = 0;
+    }
+    this.display = true;
+  }
+
+ sendVoto() {
+    console.log(this.personName, this.personCel , 'datos de usuario')
+    console.log('Valor Enviado', this.ratingValue);
+    this.display = false;
+    const restId = this.rest.id;
+    console.log(this.rest, 'id del restaurante que vote')
+
+    this.restaurantService.sendData(this.personName, this.personCel, {id : restId}, this.ratingValue)
+    .subscribe(
+      response => {
+        console.log('Respuesta del servicio de guardado:', response);
+      },
+      error => {
+        console.error('Error al guardar la respuesta:', error);
+      }
+    );
+    this.displayBye = true;
+
+  }
+  
+
+}
